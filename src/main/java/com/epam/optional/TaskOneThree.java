03bbf2857db69841cdd1dc44d348fb728241183a
neothat <neothat@mail.ru>
2020-10-19 21:55:33 +0300

    compliting third optioan-one task and create fourth task

--- src/main/java/com/epam/optional/TaskOneThree.java
+++ src/main/java/com/epam/optional/TaskOneThree.java
@@ -1,4 +1,36 @@
 package com.epam.optional;
 
+import java.util.Scanner;
+import java.lang.Math;
+
 public class TaskOneThree {
+    private static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Введите число или множество чисел через пробел");
+        String line = in.nextLine();
+        String []array = new String[6];
+        int a = 0;
+        int sum = 0;
+        for (String s : line.split(" ")) {
+            array[a] = s;
+            a++;
+        }
+        for (int i = 0; i < array.length; i++) {
+            sum = sum + array[i].length();
+        }
+        double average = sum/array.length;
+        int finalAverage = (int) Math.round(average);
+        System.out.println("Числа больше среднего значения длины: ");
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length() >= finalAverage) {
+                System.out.println(array[i]);
+            }
+        }
+        System.out.println("Числа меньше среднего значения длины: ");
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length() < finalAverage) {
+                System.out.println(array[i]);
+            }
+        }
+    }
 }
